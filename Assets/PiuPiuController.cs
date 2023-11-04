a306c7af6b7317b09073ebc20724c120764826a7
Agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-11-04 17:18:55 +0100

    Update PiuPiuController.cs
    Small fix

--- Assets/PiuPiuController.cs
+++ Assets/PiuPiuController.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class PiuPiu : MonoBehaviour
 {
     public float speed = 5f;
-    private Vector2 direction = new Vector2(1, -1).normalized; // Inicialmente se dirige hacia abajo y a la derecha
+    private Vector2 direction = new Vector2(1, -1).normalized; // Le digo que se dirija hacia abajo y a la derecha
 
     void Update()
     {
-        // Mueve a PiuPiu en la dirección actual
+        // Hago que se mueva en la dirección actual
         transform.Translate(direction * speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // Comprueba qué pared ha golpeado PiuPiu
+        // Compruebo qué pared ha golpeado PiuPiu. Las paredes son IsTrigger
         if (other.gameObject.name == "ParedInferior" || other.gameObject.name == "ParedSuperior")
         {
             // Invierte la dirección en el eje Y
